ff5bc51e6a771015c6f8c93279c216dc5e31cfce
heyjuu1208 <pajk1208@gmail.com>
2020-03-24 15:24:12 +0900

    Updates

---
+++ precache-manifest.ee9145b4b6ecd9dbf809fd058443f3b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9616144e43a3f1b984f0efa3d708d9",
+    "url": "/moive_app_2019/index.html"
+  },
+  {
+    "revision": "7e9df1c88a5748cd96cc",
+    "url": "/moive_app_2019/static/css/main.bd533123.chunk.css"
+  },
+  {
+    "revision": "9ea07e957856b93e389a",
+    "url": "/moive_app_2019/static/js/2.5338f48d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/moive_app_2019/static/js/2.5338f48d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9df1c88a5748cd96cc",
+    "url": "/moive_app_2019/static/js/main.196cd174.chunk.js"
+  },
+  {
+    "revision": "937ea5bd2e30f5ceb565",
+    "url": "/moive_app_2019/static/js/runtime-main.823b404e.js"
+  }
+]);
